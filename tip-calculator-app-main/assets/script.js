ffb36d2a8789285ff6c42a2ebb45e44b851bd983
Robrowno <robrowno@icloud.com>
2023-04-01 14:33:54 +0100

    describe plan/layout in writing in script.js

---
+++ tip-calculator-app-main/assets/script.js
@@ -0,0 +1,25 @@
+
+// Update total / person
+
+// Update tip amount / person
+
+// handle percentages
+
+// handle number of people
+
+// reset button
+
+
+/* Flow:
+
+Enter amount in 1st input
+
+Select % Tip (enter custom amount if "custom" selected)
+
+Enter number of people
+
+Both totals update automatically
+
+Reset button resets values to 0 when clicked
+
+*/
