50720d40035e9c9f0a470171e275b753432d557e
Robrowno <robrowno@icloud.com>
2023-04-02 14:52:29 +0100

    remove old/unused files, display tip percentages in the console through a handleButton function

--- tip-calculator-app-main/assets/script.js
+++ tip-calculator-app-main/assets/script.js
@@ -1,11 +1,17 @@
+// A large section of this file was with help from a youtube tutorial solution:
+// https://www.youtube.com/watch?v=etYv-pPfol4
 
 const billAmount = document.querySelector('#amount-input');
 const numberOfPeople = document.querySelector('#num-of-people');
 const totalAmount = document.querySelector('#total-counter')
 const tipPerPerson = document.querySelector('#tip-counter')
 
-const button = document.getElementsByClassName('tips')
+const buttons = document.querySelectorAll('.tips')
 const reset = document.getElementById('reset-btn')
+
+buttons.forEach(function (val) {
+    val.addEventListener('click', handleButton);
+});
 
 billAmount.addEventListener('input', billInputCalc);
 numberOfPeople.addEventListener('input', peopleAmountHandler);
@@ -19,6 +25,7 @@ tipPerPerson.innerHTML = (0.00).toFixed(2);
 
 let billValue = 0.00;
 let peopleInputValue = 1;
+let tipValue = 0.15;
 
 
 function billInputCalc() {
@@ -31,3 +38,13 @@ function peopleAmountHandler() {
     console.log(peopleInputValue);
 };
 
+function handleButton(event) {
+    buttons.forEach(function(val) {
+        val.classList.remove('tip-active');
+        if (event.target.innerHTML === val.innerHTML){
+        val.classList.add('tip-active');
+        tipValue = parseFloat(val.innerHTML)/100
+        }
+    });
+    console.log(tipValue)
+};
