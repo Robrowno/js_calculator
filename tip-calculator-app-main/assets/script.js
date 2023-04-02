f5918e24e395df1c2592dde48d5f92141841daa8
Robrowno <robrowno@icloud.com>
2023-04-02 13:43:22 +0100

    define global variables and test input event listeners through logging values in the console

--- tip-calculator-app-main/assets/script.js
+++ tip-calculator-app-main/assets/script.js
@@ -1,25 +1,33 @@
 
-// Update total / person
+const billAmount = document.querySelector('#amount-input');
+const numberOfPeople = document.querySelector('#num-of-people');
+const totalAmount = document.querySelector('#total-counter')
+const tipPerPerson = document.querySelector('#tip-counter')
 
-// Update tip amount / person
+const button = document.getElementsByClassName('tips')
+const reset = document.getElementById('reset-btn')
 
-// handle percentages
-
-// handle number of people
-
-// reset button
+billAmount.addEventListener('input', billInputCalc);
+numberOfPeople.addEventListener('input', peopleAmountHandler);
 
 
-/* Flow:
+billAmount.value = "0.00";
+numberOfPeople.value = "1";
+totalAmount.innerHTML = (0.00).toFixed(2);
+tipPerPerson.innerHTML = (0.00).toFixed(2);
 
-Enter amount in 1st input
 
-Select % Tip (enter custom amount if "custom" selected)
+let billValue = 0.00;
+let peopleInputValue = 1;
 
-Enter number of people
 
-Both totals update automatically
+function billInputCalc() {
+    billValue = parseFloat(billAmount.value);
+    console.log(billValue);
+};
 
-Reset button resets values to 0 when clicked
+function peopleAmountHandler() {
+    peopleInputValue = parseFloat(numberOfPeople.value);
+    console.log(peopleInputValue);
+};
 
-*/
